d92e88061c3aca46b6af430977d7cb85bbaae155
donkorgodfred634-sketch <donkorgodfred634@gmail.com>
2026-05-30 23:52:38 +0000

    Submit initials assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: OTOO BENJAMIN
+# STUDENT ID: 2425401453
+# Email: 2425401453@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, OTOO BENJAMIN, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+ # Print initials
 
+  print()
+  print("...DBO")
+  print()
+  print("...DDDDDDDDD...........BBBBBBBBB...........OOOOOOOOOO.......")
+  print("...D.........DD........B.........BB........O..........O.....")
+  print("...D..........D........B..........B........O..........O.....")
+  print("...D..........D........B.........BB........O..........O.....")
+  print("...D..........D...**...BBBBBBBBB......**...O..........O...**")
+  print("...D..........D...**...B.........BB...**...O..........O...**")
+  print("...D..........D........B..........B........O..........O.....")
+  print("...D..........D........B..........B........O..........O.....")
+  print("...D.........DD........B.........BB........O..........O.....")
+  print("...DDDDDDDDD...........BBBBBBBBB...........OOOOOOOOOO.......")
+  print()
 
 
 main()
